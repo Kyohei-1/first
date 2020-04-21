31670e3d6710f2ba94d1bd573c5cf93714bb9f6a
Kyohei-1 <62252628+Kyohei-1@users.noreply.github.com>
2020-04-21 12:07:33 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -354,27 +354,40 @@
 #   r = random.randint(1,6)
 #   print('サイコロの目は'+str(r)+'です。')
 
-import random
-hand = ['グー','チョキ','パー','ゲーム終了']
-print('------じゃんけんゲーム------')
-while True:
-  com = random.randint(0,2)
-  for i,desc in enumerate(hand):
-    print(i,':',desc)
-  you = int(input('出す手を数値で入力'))
-  if you == 3: break
-  if you < 0 or you > 2:
-    print('0から3の間で入力してね')
-    continue
-  print('---')
-  print('自分：',hand[you])
-  print('相手：', hand[com])
-  input('---')
-  j = (you - com + 3) % 3
-  if j == 0:
-    print('あいこ')
-  elif j == 1:
-    print('負け')
-  else:
-    print('勝ち')
-  input('---')
+# import random
+# hand = ['グー','チョキ','パー','ゲーム終了']
+# print('------じゃんけんゲーム------')
+# while True:
+#   com = random.randint(0,2)
+#   for i,desc in enumerate(hand):
+#     print(i,':',desc)
+#   you = int(input('出す手を数値で入力'))
+#   if you == 3: break
+#   if you < 0 or you > 2:
+#     print('0から3の間で入力してね')
+#     continue
+#   print('---')
+#   print('自分：',hand[you])
+#   print('相手：', hand[com])
+#   input('---')
+#   j = (you - com + 3) % 3
+#   if j == 0:
+#     print('あいこ')
+#   elif j == 1:
+#     print('負け')
+#   else:
+#     print('勝ち')
+#   input('---')
+
+# import datetime
+
+# def print_d(string):
+#   print(string)
+
+# now = datetime.date.today()
+# print_d(now)
+
+# import math
+
+# print(math.pi)
+
