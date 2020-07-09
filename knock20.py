ef4fcbdd54bb68471a1b87e60a685e55edf5995e
Kyohei-1 <62252628+Kyohei-1@users.noreply.github.com>
2020-07-09 09:36:23 +0900

    20最後に空白行ありに訂正

--- knock20.py
+++ knock20.py
@@ -1,25 +1,15 @@
 # coding: utf-8
-
-import pandas as pd
+import json
+import gzip
 import sys
 
-path = sys.argv[1]
-# ファイルのパス
+filePath = sys.argv[1]
+with open('./KF20.txt', 'w') as wFile:
+    with gzip.open(filePath, "r") as file:
+        for line in file:
+            article = json.loads(line)
+            if article["title"] == "イギリス":
+                wFile.write(article["text"]+'\n')
 
-df = pd.read_json(path, lines=True)
-# lines=Trueにしないとエラーになるよ
-
-print(df.query("title == 'イギリス'")["text"].values[0])
-# # SQLみたいなノリでかけるやつ？
-
-# # print(df)
-
-with open('./knock20_result.txt', 'w') as file:
-    file.write(df.query("title == 'イギリス'")["text"].values[0])
-    # ファイルに書き込む。クエリを投げれる方法を見つけたのでそれで。
-    # titleの部分に国名があったので、とりあえずこれを取得していく。
-    # 形式は key valueだと思うので、
-    # 国名（title）が'イギリス'
-    # 内容（text）というキーの中身（values[0])を取得して書き込んでく。
 
 # python knock20.py ./jawiki-country.json
