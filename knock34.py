23363dcd141a19c347e2c1b9bd6b352bc769dcba
Kyohei-1 <62252628+Kyohei-1@users.noreply.github.com>
2020-07-26 03:15:19 +0900

    Add files via upload

---
+++ knock34.py
@@ -0,0 +1,59 @@
+txt = './KF_neko.txt.mecab'
+mapData = {}
+listData = []
+tmpText1 = '2020'
+tmpText2 = '2020'
+# tmpText3 = '2020'
+counter = 0
+tmpText_flg1 = False
+tmpText_flg2 = False
+# tmpText_flg3 = False
+
+with open('./KF34.txt', 'w') as writeFile:
+    with open(txt, 'r') as readFile:
+        for text in readFile:
+            # print(text)
+            # \tで区切って先頭だけ見る
+            listData = text.split('\t')
+            # 表層形
+            surface = listData[0]
+            # EOSが入ってたら消す
+            if surface == ['EOS\n']:
+                surface = ''
+            # print(surface)
+            # 表層形以外をバラす
+            splitted = listData[-1].split(',')
+            # EOSが入ってたら消す
+            if splitted == ['EOS\n']:
+                continue
+            else:
+                # 前の分が名詞「一般」なら取得
+                pos = splitted[0]
+                pos1 = splitted[1]
+
+                if tmpText_flg1 == True:
+                    if pos in '名詞':
+                        tmpText2 = text[0]
+                        tmpText_flg2 = True
+                        if tmpText_flg2:
+                            writeFile.write(tmpText1+tmpText2+'\n')
+
+                if pos in '名詞':
+                    tmpText1 = text[0]
+                    tmpText_flg1 = True
+                    # print(tmpText1)
+
+                    # if '2020' not in tmpText1 and '2020' not in tmpText2 and '2020' not in tmpText3:
+                    #     writeFile.write(tmpText1+tmpText2+tmpText3+'\n')
+                # # 恐らく助詞で連体化の「の」を取ってくればいい
+                # # その前と後ろを含めて取得
+                # if pos in ('助詞'):
+                #     # writeFile.write('SUCCESS')
+                #     if pos1 in ('連体化'):
+                #         # writeFile.write('SUCCESS2')
+                #         # 助詞で連体化の「の」
+                #         prevMeisi = splitted[5]
+                #         zyosiRentai = splitted[6]
+                #         nextMeisi = splitted[7]
+                #         zyosiRentai = prevMeisi+nextMeisi
+                #         writeFile.write(zyosiRentai+'\n')
