c65b875847d2c159d16c5c2809eb0c4375bffeea
Kyohei-1 <62252628+Kyohei-1@users.noreply.github.com>
2020-08-06 13:51:24 +0900

    後は '猫' 以外の単語ごとに分割してリストにブチ込んでCounterに渡すだけ...
    もしかしてすっごい無駄なことしてる？

--- knock37.py
+++ knock37.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import japanize_matplotlib
 
 txt = './KF_neko.txt.mecab'
-mapData = {}
+# mapData = {}
 data = ''
-
+neko = []
 
 with open('./KF37.txt', 'w') as writeFile:
     with open(txt, 'r') as readFile:
@@ -26,10 +26,21 @@ with open('./KF37.txt', 'w') as writeFile:
         data = data.split()
         data = ''.join(data)
         data = ''.join(data).split('EOS')
-        if '猫' in data:
-            print(data)
-        else:
-            print('Bad')
+        for bun in data:
+           # dataはlist 
+           # print(type(data))
+           # bunは文字列
+           # print(type(bun))
+           if '猫' in bun:
+               # 猫が含まれている行を抽出してリストに入れる
+               neko.append(bun)
+               print(neko)
+
+        # こっから下は気にしないで。#
+        # print(data)
+        # print(any(x is e or x == e for e in y))
+        # if '猫' not in data:
+             # print(data)
         # data = ''.join(data).split('EOS')
         # print(data)
         
